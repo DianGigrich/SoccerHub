12b876be21d32160041560e4296e19b9a66372b9
Dian Sherman <slayer_barrett_@hotmail.com>
2023-01-12 13:47:00 -0800

    added header

---
+++ src/components/Footer.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+
+
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="text.secondary">
+//       {'Copyright © '}
+//        <Link color="inherit" href="https://github.com/DianGigrich/Codescape/">
+//        Codescape
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+export default function StickyFooter({puzzleImage}) {
+  if (puzzleImage===true) {
+    return (<></>)
+  } else {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+      
+    >
+ 
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          position:"fixed",
+          bottom:0
+          
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography  variant="body1" gutterBottom>
+            Thank you for playing today!
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </Box>
+  );
+}
+}
