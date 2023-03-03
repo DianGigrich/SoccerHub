ab49ecddb402f3b2adf7d85672ff864ce14b21b7
Dian Sherman <slayer_barrett_@hotmail.com>
2023-03-02 17:20:54 -0800

    changed cancelled on calendar

--- src/components/pages/Calendar.js
+++ src/components/pages/Calendar.js
@@ -6,9 +6,9 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 export default function Calendar(props) {
  const info = {events: [
         { title: 'Practice Game', date: '2023-03-04', time: '' },
-        { title: 'Practice Cancelled', date: '2023-03-14' },
-        { title: 'Practice Cancelled', date: '2023-03-21' },
-        { title: 'Practice Cancelled?', date: '2023-03-15'},
+        { title: 'Cancelled', date: '2023-03-14' },
+        { title: 'Cancelled', date: '2023-03-21' },
+        { title: 'Cancelled?', date: '2023-03-15'},
         { title: '2pm BHS 8', date: '2023-03-11'},
         { title: '9am BHS 8', date: '2023-03-18'},
         { title: '11:30am BHS 8', date: '2023-03-25'},
