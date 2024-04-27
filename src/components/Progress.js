8d43c53804978b2efe35bbe5685c4b8ebddc32e3
Dian Sherman <slayer_barrett_@hotmail.com>
2024-04-27 11:45:48 -0700

    removed unnecessary line

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { LinearProgress, Box, Typography } from "@mui/material";
-import { useState } from "react";
 
 Progress.propTypes = {
     /**
